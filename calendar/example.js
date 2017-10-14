3fd97efc81ff6ac62067135cfe4c995ffb591aee
Vicky Zhu <vii.zhu@gmail.com>
2017-10-14 15:36:04 -0400

    edited formatting

--- calendar/example.js
+++ calendar/example.js
@@ -9,14 +9,14 @@ angular
 
     vm.externalEvents = [
       {
-        title: 'Event 1',
+        title: 'Recipe 1',
         type: 'warning',
         color: calendarConfig.colorTypes.warning,
         startsAt: moment().startOf('month').toDate(),
         draggable: true
       },
       {
-        title: 'Event 2',
+        title: 'Recipe 2',
         type: 'danger',
         color: calendarConfig.colorTypes.important,
         startsAt: moment().startOf('month').toDate(),
@@ -40,6 +40,48 @@ angular
       }
       vm.viewDate = start;
       vm.cellIsOpen = true;
+    };
+
+    vm.eventClicked = function(event) {
+          alert.show('Clicked', event);
+        };
+
+    vm.eventEdited = function(event) {
+      alert.show('Edited', event);
+    };
+
+    vm.eventDeleted = function(event) {
+      alert.show('Deleted', event);
+    };
+
+    vm.eventTimesChanged = function(event) {
+      alert.show('Dropped or resized', event);
+    };
+
+    vm.toggle = function($event, field, event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      event[field] = !event[field];
+    };
+
+    vm.timespanClicked = function(date, cell) {
+
+      if (vm.calendarView === 'month') {
+        if ((vm.cellIsOpen && moment(date).startOf('day').isSame(moment(vm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
+          vm.cellIsOpen = false;
+        } else {
+          vm.cellIsOpen = true;
+          vm.viewDate = date;
+        }
+      } else if (vm.calendarView === 'year') {
+        if ((vm.cellIsOpen && moment(date).startOf('month').isSame(moment(vm.viewDate).startOf('month'))) || cell.events.length === 0) {
+          vm.cellIsOpen = false;
+        } else {
+          vm.cellIsOpen = true;
+          vm.viewDate = date;
+        }
+      }
+
     };
 
   });
