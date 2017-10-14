0519348ebb16ca5b32ff480966cdc3e49229594b
Karen Reeves <kgentsch@gmail.com>
2017-10-14 14:42:01 -0400

    Adding calendar directory and shell of calendar

---
+++ calendar/example.js
@@ -0,0 +1,45 @@
+angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module']);
+angular
+  .module('mwl.calendar.docs')
+  .controller('DraggableExternalEventsCtrl', function(moment, calendarConfig) {
+
+    var vm = this;
+
+    vm.events = [];
+
+    vm.externalEvents = [
+      {
+        title: 'Event 1',
+        type: 'warning',
+        color: calendarConfig.colorTypes.warning,
+        startsAt: moment().startOf('month').toDate(),
+        draggable: true
+      },
+      {
+        title: 'Event 2',
+        type: 'danger',
+        color: calendarConfig.colorTypes.important,
+        startsAt: moment().startOf('month').toDate(),
+        draggable: true
+      }
+    ];
+
+    vm.calendarView = 'month';
+    vm.viewDate = moment().startOf('month').toDate();
+    vm.cellIsOpen = false;
+
+    vm.eventDropped = function(event, start, end) {
+      var externalIndex = vm.externalEvents.indexOf(event);
+      if (externalIndex > -1) {
+        vm.externalEvents.splice(externalIndex, 1);
+        vm.events.push(event);
+      }
+      event.startsAt = start;
+      if (end) {
+        event.endsAt = end;
+      }
+      vm.viewDate = start;
+      vm.cellIsOpen = true;
+    };
+
+  });
